5f5a8364b70d17c4c517e5460dc7867c50199b18
manteleannkatherine@outlook.com <manteleannkatherine@outlook.com>
2021-09-15 11:28:23 +0400

    Update apollo landing page

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import 'reflect-metadata';
 import { PrismaClient } from '@prisma/client';
 import { ApolloServer } from 'apollo-server';
 import { resolvers } from "./generated/typegraphql-prisma";
-import {
-  ApolloServerPluginLandingPageProductionDefault,
-  ApolloServerPluginLandingPageLocalDefault
+import { ApolloServerPluginLandingPageLocalDefault,
+         ApolloServerPluginLandingPageGraphQLPlayground
 } from "apollo-server-core";
 import * as tq from 'type-graphql';
 
@@ -13,12 +12,7 @@ const prisma = new PrismaClient()
 const PORT = process.env.PORT || 4000
 
 const app = async () => {
-  const schema = await tq.buildSchema({ resolvers,
-  plugins: [
-    process.env.NODE_ENV === 'production' ?
-      ApolloServerPluginLandingPageProductionDefault({ footer: false }) :
-      ApolloServerPluginLandingPageLocalDefault({ footer: false })
-  ] });
+  const schema = await tq.buildSchema({ resolvers });
 
   const context = () => {
     return {
@@ -26,7 +20,16 @@ const app = async () => {
     }
   }
   
-  new ApolloServer({ schema, context }).listen({ port: PORT }, () =>
+  new ApolloServer({ 
+    schema, 
+    context, 
+    plugins: [
+      // Install a landing page plugin based on NODE_ENV
+      process.env.NODE_ENV === 'production'
+        ? ApolloServerPluginLandingPageGraphQLPlayground()
+        : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    ],
+  }).listen({ port: PORT }, () =>
     console.log(`app running on port ${PORT}`)
   )
 }
